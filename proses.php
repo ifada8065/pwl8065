54fc4361afcf654700c447172dce6d3bde67d0cd
ifadatik <datik8065@students.amikom.ac.id>
2016-11-19 11:57:13 +0700

    proses submit 1 edit
    submit dengan Metode GET

--- proses.php
+++ proses.php
@@ -11,7 +11,7 @@ if(isset($_GET['fnama']))
 {
  $fnama = $_GET['fnama'];
  $lalamat = $_GET['lalamat'];
- echo "<span class='success'>Dengan <b>METODE GET</b></span><br/>";
+ echo "<span class='success'><br/>Dengan <b>Metode G</b></span><br/>";
  echo "Nama : ".$fnama."<br/>Alamat : ".$lalamat;
 }
 ?>
